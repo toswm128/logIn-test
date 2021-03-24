5efdb9382c15db98917bba72062461eb952d526c
toswm128 <chominsu10@gmail.com>
2021-03-24 18:08:18 +0900

    [ADD] autoLogin

--- login/src/containers/HomeContainer.js
+++ login/src/containers/HomeContainer.js
@@ -1,34 +1,43 @@
 import React,{useState} from "react"
-import {Link} from "react-router-dom"
 import axios from "axios";
 import Home from "../components/Home/Home"
+
+
+const getUsers = async () =>{
+    try{
+        const userId = "test";
+        const getUser = await axios.post(`https://noons.herokuapp.com/user/${userId}`,{},{
+                headers: {
+                    'Authorization': localStorage.getItem("accessToken")
+                }
+            })
+    }catch(error){
+
+    }
+}
 
 const autoLog = async () =>{
     try{
         console.log(localStorage.getItem("accessToken"))
-        const autoLogin = await axios.post("https://noons.herokuapp.com/autosignin",{
+        const autoLogin = await axios.post("https://noons.herokuapp.com/autosignin",{},{
             headers: {
-                Authorization: localStorage.getItem("accessToken")
+                'Authorization': localStorage.getItem("accessToken")
             }
         })
         console.log(autoLogin)
-        alert("로그인 성공!")
     }catch(error){
         console.log(error.response.status);
     }
 }
 
 function HomeContainer(){
-    const [count, setcount] = useState(0);
     const autoLoginChecked = JSON.parse(localStorage.getItem("autoLogin"))
     if(autoLoginChecked){
         autoLog()
     }
+    
     return(
-        <Home 
-            count = {count}
-            setcount = {setcount}
-        />
+        <Home />
     )
 }
 
