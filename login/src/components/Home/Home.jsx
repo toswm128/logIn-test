4987057e674ee5e8ca123f048d003f299a60a0e7
toswm128 <chominsu10@gmail.com>
2021-03-29 11:00:22 +0900

    [ADD] banner

--- login/src/components/Home/Home.jsx
+++ login/src/components/Home/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react"
 import {Link} from "react-router-dom"
 import "./Home.css"
+import banner from "../../styles/imgs/banner.png"
 
 function Home({
 }){
     return(
         <div className="Home-body">
             <div className="banner">
-                <img src="../../styles/imgs/banner.png" alt="banner" width="200" />
+                <img className="banner" src={banner} alt="banner" />
             </div>
             <div className ="link">
                 <Link to="/logIn">logIn</Link>
